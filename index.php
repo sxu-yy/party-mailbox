d51ffa4d6464a46048c3390fb2e5f10b3f1637cf
sxu-yy <52720183+sxu-yy@users.noreply.github.com>
2025-08-03 17:41:10 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  $fname = "mailbox.txt";
+  if (!empty($_POST)) {
+    $mail = htmlspecialchars($_POST['tosend']);
+    $fh = fopen($fname, 'a') or die("Can't open file");
+    fwrite($fh, $mail);
+    fwrite($fh, "\n");
+    fclose($fh);
+  }
+
+  $fh = fopen($fname, 'r');
+  while (($contents = fgets($fh, 4096)) !== false) {
+    echo $contents;
+    echo "<br>";
+  }
+  fclose($fh);
+?>
